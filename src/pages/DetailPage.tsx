2afce9003d33fc3e81f03b4346d4f5fbf7e8b7e4
jhoon9494 <wjdgns9406@gmail.com>
2022-10-17 19:18:37 +0900

    Feat: 게시글의 해시태그와 해시태그 검색 결과 컴포넌트 연결
    1. 해시태그 클릭 시 해시태그 검색 결과 컴포넌트로 이동

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -1,14 +1,14 @@
 import styled from 'styled-components';
 import { useState, useEffect } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useNavigate, useParams, Link } from 'react-router-dom';
 import ScoreBox from 'components/atoms/ScoreBox';
 import BackSpaceBtn from 'components/atoms/BackSpaceBtn';
 import ImgSlider from 'components/organism/ImgSlider';
 import HeartBtn from 'components/atoms/HeartBtn';
-import Route from 'components/organism/Route';
+import TravelTips from 'components/organism/TravelTips';
 import axios from 'axios';
 
-type RoutesType = {
+type TravelTips = {
   placeName: string;
   tips: string;
 };
@@ -22,7 +22,7 @@ type FiguresType = {
 interface PostData {
   id: string;
   post_img: string[];
-  recommendRoutes: RoutesType[] | [];
+  recommendRoutes: TravelTips[] | [];
   title: string;
   content: string;
   hashTags: string[];
@@ -45,6 +45,21 @@ const initialSet: PostData = {
   like: 0,
 };
 
+// 게시글의 본문 및 해시태그 생성 함수
+function createContent(content: string) {
+  return content.split(/(#[^\s#]+)/g).map((str, index) => {
+    if (str[0] === '#') {
+      const hashtag = str.split('#')[1];
+      return (
+        <Link to={`/hashtag/${hashtag}`} key={`${index + 1}-hashtag`}>
+          {str}
+        </Link>
+      );
+    }
+    return str;
+  });
+}
+
 function DetailPage() {
   const [data, setData] = useState<PostData>(initialSet);
   const navigate = useNavigate();
@@ -65,14 +80,14 @@ function DetailPage() {
       <DataContainer>
         <PostContainer>
           <ImgSlider img={data.post_img} />
-          {/* TODO 좋아요 api 요청할 때 필요한 데이터 넘겨줘야하고 관련데이터 타입 정하기 */}
+          {/* TODO 좋아요 클릭 시 받아온 좋아요 데이터에서 +1, -1 해서 다시 넘겨주기 */}
           <HeartBtn heartNum={data.like} />
           {data.recommendRoutes.length !== 0 ? (
-            <Route routes={data.recommendRoutes} />
+            <TravelTips TravelTips={data.recommendRoutes} />
           ) : (
             <div style={{ height: '40px' }} />
           )}
-          <TextArea>{data.content}</TextArea>
+          <TextArea>{createContent(data.content)}</TextArea>
         </PostContainer>
 
         <SideContainer>
