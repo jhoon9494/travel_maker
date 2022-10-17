d614531496975587446d043568106b365c7f24a4
jhoon9494 <wjdgns9406@gmail.com>
2022-10-17 17:50:35 +0900

    Fix: 메인페이지, 로그인페이지 라우터 주소 변경

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Login from 'pages/Login';
 import Register from 'pages/Register';
 import DetailPage from 'pages/DetailPage';
 import Upload from 'pages/Upload';
+import HashtagList from 'pages/HashtagList';
 
 function App() {
   return (
@@ -14,11 +15,13 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route element={<Layout />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
+            <Route path="/main" element={<Home />} />
+            <Route path="/" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/detail/:id" element={<DetailPage />} />
             <Route path="/upload" element={<Upload />} />
+            <Route path="/hashtag/:tag" element={<HashtagList />} />
+            {/* TODO 404 Not Found 페이지 만들기 */}
           </Route>
         </Routes>
       </BrowserRouter>
