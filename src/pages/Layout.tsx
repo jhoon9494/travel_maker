0844a015097aed813bb541fc0b3c751e6777801b
jhoon9494 <wjdgns9406@gmail.com>
2022-10-18 19:27:47 +0900

    Refactor: 로그인, 회원가입 페이지 레이아웃 컴포넌트에서 제외

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,12 +1,11 @@
-import { Outlet, useLocation } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import Navbar from 'components/organism/Navbar';
 import styled from 'styled-components';
 
 function Layout() {
-  const location = useLocation();
   return (
     <Wrapper>
-      {location.pathname !== '/' && location.pathname !== '/register' ? <Navbar /> : <NonNavbar />}
+      <Navbar />
       <Body>
         <Outlet />
       </Body>
@@ -19,11 +18,6 @@ export default Layout;
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
-`;
-
-const NonNavbar = styled.div`
-  background-color: white;
-  height: 65px;
 `;
 
 const Body = styled.div`
