281f74291e5de0a87bee8c51571b59f3c82c5557
jhoon9494 <wjdgns9406@gmail.com>
2022-11-21 12:23:13 +0900

    Refactor: 무한스크롤 코드 리팩토링 및 로딩 컴포넌트 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,7 @@ import { useCallback, useEffect, useRef, useState } from 'react';
 import axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom';
 import { BiEdit } from 'react-icons/bi';
+import Loading from '../components/atoms/Loading';
 
 type FiguresType = {
   recommend: number;
@@ -22,11 +23,11 @@ type PostData = {
 
 function Home() {
   const navigate = useNavigate();
-  // TODO 데이터 몇개씩 뿌려줄 예정???? 일단 4개로 만들어둠
+  const [isLoading, setIsLoading] = useState(false);
+  // TODO 데이터 몇개씩 뿌려줄 예정????
   const [postList, setPostList] = useState<PostData[]>([]);
 
   // 무한 스크롤 관련 부분
-  const observerRef = useRef<IntersectionObserver>();
   const boxRef = useRef<HTMLDivElement>(null);
 
   const getData = useCallback(async () => {
@@ -36,30 +37,37 @@ function Home() {
     } catch (e: any) {
       console.error(e);
       // TODO 무한스크롤로 더이상 받아올 정보가 없는 경우에는 기존에 모아진 배열 그대로 다시 반환하면 될듯
+      // 추가로 boxRef.current = null로 변경해서 옵저버 관찰 없애기
     }
   }, []);
 
   useEffect(() => {
+    setIsLoading(true);
     getData();
+    setIsLoading(false);
   }, [getData]);
+
+  const intersectionObserver = useCallback(
+    (entries: IntersectionObserverEntry[], observer: IntersectionObserver) => {
+      entries.forEach((entry) => {
+        // 관찰대상 entry가 화면에 보여지는 경우 실행
+        if (entry.isIntersecting) {
+          observer.unobserve(entry.target); // entry 관찰 해제
+          getData(); // 데이터 가져오기
+        }
+      });
+    },
+    [getData],
+  );
 
   // postList가 갱신될 때마다 observer 설정
   useEffect(() => {
-    observerRef.current = new IntersectionObserver(intersectionObserver); // IntersectionObserver
-    // eslint-disable-next-line
-    boxRef.current && observerRef.current.observe(boxRef.current);
-    // eslint-disable-next-line
-  }, [postList]);
-
-  const intersectionObserver = (entries: IntersectionObserverEntry[], io: IntersectionObserver) => {
-    entries.forEach((entry) => {
-      // 관찰대상 entry가 화면에 보여지는 경우 실행
-      if (entry.isIntersecting) {
-        io.unobserve(entry.target); // entry 관찰 해제
-        getData(); // 데이터 가져오기
-      }
-    });
-  };
+    let io;
+    if (boxRef.current) {
+      io = new IntersectionObserver(intersectionObserver); // IntersectionObserver
+      io.observe(boxRef.current);
+    }
+  }, [postList, intersectionObserver]);
 
   const handleMoveContent = (postId: string) => {
     navigate(`/detail/${postId}`);
@@ -67,55 +75,56 @@ function Home() {
 
   return (
     <Wrapper>
-      {postList.length === 0 ? (
+      {/* eslint-disable */}
+      {isLoading ? (
+        <Loading />
+      ) : postList.length === 0 ? (
         <EmptyPost>
           <h2>표시할 게시글이 없습니다.</h2>
           <h2>다른 유저를 팔로우 해주세요!</h2>
         </EmptyPost>
       ) : (
-        <>
-          {postList.map((data, index) => {
-            // 마지막 게시글을 담고 있는 컴포넌트에 무한스크롤을 적용시키기 위해 ref를 담아줌
-            if (postList.length - 1 === index) {
-              return (
-                <div ref={boxRef} key={`${data.title}-${index + 1}-key`}>
-                  <Post>
-                    <Img
-                      src={data.post_img[0]}
-                      alt={`${data.title}-1번째 이미지`}
-                      onClick={() => handleMoveContent(data.id)}
-                    />
-                    <PostText>
-                      <H2Tag onClick={() => handleMoveContent(data.id)}>{data.title}</H2Tag>
-                      <p>
-                        <Link to={`/${data.user_id}`}>{data.user_id}</Link>
-                      </p>
-                    </PostText>
-                  </Post>
-                </div>
-              );
-            }
+        postList.map((data, index) => {
+          // 마지막 게시글을 담고 있는 컴포넌트에 무한스크롤을 적용시키기 위해 ref를 담아줌
+          if (postList.length - 1 === index) {
             return (
-              <Post key={`${data.title}-${index + 1}-key`}>
-                <Img
-                  src={data.post_img[0]}
-                  alt={`${data.title}-1번째 이미지`}
-                  onClick={() => handleMoveContent(data.id)}
-                />
-                <PostText>
-                  <H2Tag onClick={() => handleMoveContent(data.id)}>{data.title}</H2Tag>
-                  <p>
-                    <Link to={`/${data.user_id}`}>{data.user_id}</Link>
-                  </p>
-                </PostText>
-              </Post>
+              <div ref={boxRef} key={`${data.title}-${index + 1}-key`}>
+                <Post>
+                  <Img
+                    src={data.post_img[0]}
+                    alt={`${data.title}-1번째 이미지`}
+                    onClick={() => handleMoveContent(data.id)}
+                  />
+                  <PostText>
+                    <H2Tag onClick={() => handleMoveContent(data.id)}>{data.title}</H2Tag>
+                    <p>
+                      <Link to={`/${data.user_id}`}>{data.user_id}</Link>
+                    </p>
+                  </PostText>
+                </Post>
+              </div>
             );
-          })}
-          <UploadBtn onClick={() => navigate('/upload')}>
-            <BiEdit />
-          </UploadBtn>
-        </>
+          }
+          return (
+            <Post key={`${data.title}-${index + 1}-key`}>
+              <Img
+                src={data.post_img[0]}
+                alt={`${data.title}-1번째 이미지`}
+                onClick={() => handleMoveContent(data.id)}
+              />
+              <PostText>
+                <H2Tag onClick={() => handleMoveContent(data.id)}>{data.title}</H2Tag>
+                <p>
+                  <Link to={`/${data.user_id}`}>{data.user_id}</Link>
+                </p>
+              </PostText>
+            </Post>
+          );
+        })
       )}
+      <UploadBtn onClick={() => navigate('/upload')}>
+        <BiEdit />
+      </UploadBtn>
     </Wrapper>
   );
 }
@@ -127,6 +136,8 @@ const Wrapper = styled.div`
   flex-direction: column;
   background-color: white;
   min-height: 100%;
+  justify-content: center;
+  align-items: center;
 `;
 
 const EmptyPost = styled.div`
