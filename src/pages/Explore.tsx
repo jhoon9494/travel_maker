3f9678e891d96c2ad99d39ea5f8564b1194811be
jhoon9494 <wjdgns9406@gmail.com>
2022-12-04 15:24:03 +0900

    Refactor: 변수명 수정 및 로딩 컴포넌트 추가

--- src/pages/Explore.tsx
+++ src/pages/Explore.tsx
@@ -3,6 +3,7 @@ import { useParams, useNavigate, Link } from 'react-router-dom';
 import BackSpaceBtn from 'components/atoms/BackSpaceBtn';
 import { useCallback, useEffect, useState } from 'react';
 import axios from 'axios';
+import Loading from '../components/atoms/Loading';
 
 type SearchDataType = {
   id: string;
@@ -14,38 +15,41 @@ type TagDataType = {
 };
 
 function Explore() {
-  const [searchData, setSearchData] = useState<SearchDataType[]>([]);
-  const [hashtagData, setHashtagData] = useState<TagDataType[]>([]);
   const { result } = useParams();
   const navigate = useNavigate();
+  const [searchData, setSearchData] = useState<SearchDataType[]>([]);
+  const [hashtagData, setHashtagData] = useState<TagDataType[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const getData = useCallback(() => {
     setSearchData([]);
     setHashtagData([]);
 
     Promise.allSettled([
-      axios.get('http://localhost:8888/api/user', { params: { word: result }, withCredentials: true }),
-      axios.get('http://localhost:8888/api/tag', { params: { word: result }, withCredentials: true }),
+      axios.get('/api/user', { params: { word: result } }),
+      axios.get(`/api/post/tag/${result}`),
     ]).then((res) =>
       res.forEach((resData, index) => {
         // 유저 검색 부분
         if (index === 0 && resData.status === 'fulfilled') {
-          resData.value.data.forEach((user: { user_id: string; profile_img: string }) => {
-            setSearchData((prev) => [...prev, { id: user.user_id, img: user.profile_img }]);
+          resData.value.data.forEach((user: { userId: string; profileImg: string }) => {
+            setSearchData((prev) => [...prev, { id: user.userId, img: user.profileImg }]);
           });
         }
-        // 해시태그 검색 부분
-        if (index === 1 && resData.status === 'fulfilled') {
-          resData.value.data.forEach((tag: TagDataType) => {
-            setHashtagData((prev) => [...prev, tag]);
-          });
-        }
+        //   // 해시태그 검색 부분
+        //   if (index === 1 && resData.status === 'fulfilled') {
+        //     resData.value.data.forEach((tag: TagDataType) => {
+        //       setHashtagData((prev) => [...prev, tag]);
+        //     });
+        //   }
       }),
     );
   }, [result]);
 
   useEffect(() => {
+    setIsLoading(true);
     getData();
+    setIsLoading(false);
   }, [getData]);
 
   return (
@@ -53,37 +57,47 @@ function Explore() {
       <BackSpaceBtn onClick={() => navigate(-1)} />
       <ResultWrapper>
         <h3>유저 검색 결과 </h3>
-        <UserDataContainer>
-          {searchData.length !== 0 ? (
-            searchData.map((data, index) => {
-              return (
-                <Link to={`/${data.id}`} key={`${data.id}-${index + 1}`}>
-                  <UserData>
-                    <img src={data.img} alt="검색된 유저 이미지" />
-                    <p>{data.id}</p>
-                  </UserData>
-                </Link>
-              );
-            })
-          ) : (
-            <p>검색 결과가 없습니다.</p>
-          )}
-        </UserDataContainer>
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <UserDataContainer>
+            {searchData.length !== 0 ? (
+              searchData.map((data, index) => {
+                return (
+                  <Link to={`/${data.id}`} key={`${data.id}-${index + 1}`}>
+                    <UserData>
+                      <img src={data.img} alt="검색된 유저 이미지" />
+                      <p>{data.id}</p>
+                    </UserData>
+                  </Link>
+                );
+              })
+            ) : (
+              <p>검색 결과가 없습니다.</p>
+            )}
+          </UserDataContainer>
+        )}
       </ResultWrapper>
       <ResultWrapper>
         <h3>태그 검색 결과</h3>
-        {hashtagData.length !== 0 ? (
-          <TagData>
-            {hashtagData.map((data, index) => {
-              return (
-                <p key={`${data.tag}-${index + 1}`}>
-                  <Link to={`/hashtag/${data.tag}`}>#{data.tag}</Link>
-                </p>
-              );
-            })}
-          </TagData>
+        {isLoading ? (
+          <Loading />
         ) : (
-          <p style={{ margin: '20px 0' }}>검색 결과가 없습니다.</p>
+          <div>
+            {hashtagData.length !== 0 ? (
+              <TagData>
+                {hashtagData.map((data, index) => {
+                  return (
+                    <p key={`${data.tag}-${index + 1}`}>
+                      <Link to={`/hashtag/${data.tag}`}>#{data.tag}</Link>
+                    </p>
+                  );
+                })}
+              </TagData>
+            ) : (
+              <p style={{ marginBottom: '20px' }}>검색 결과가 없습니다.</p>
+            )}
+          </div>
         )}
       </ResultWrapper>
     </Wrapper>
@@ -100,13 +114,17 @@ const Wrapper = styled.div`
 
 const ResultWrapper = styled.div`
   padding: 20px 50px 50px;
+
+  > h3 {
+    margin-bottom: 20px;
+  }
 `;
 
 const UserDataContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
   width: 100%;
-  margin: 20px 0;
+  margin-bottom: 20px;
 `;
 
 const UserData = styled.div`
